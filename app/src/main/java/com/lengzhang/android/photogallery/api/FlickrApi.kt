dd2593a919973f142e43a3b9e6d5cdb06c456610
Leng <lengzhang@live.com>
2021-03-14 11:51:19 -0700

    Searching Flickr

--- app/src/main/java/com/lengzhang/android/photogallery/api/FlickrApi.kt
+++ app/src/main/java/com/lengzhang/android/photogallery/api/FlickrApi.kt
@@ -3,21 +3,18 @@ package com.lengzhang.android.photogallery.api
 import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Query
 import retrofit2.http.Url
 
-private const val apiKey = "f608b045255d6b1bc207d7a7b60a7a95"
 
 interface FlickrApi {
 
-    @GET(
-        "services/rest/?method=flickr.interestingness.getList" +
-                "&api_key=" + apiKey +
-                "&format=json" +
-                "&nojsoncallback=1" +
-                "&extras=url_s"
-    )
+    @GET("services/rest?method=flickr.interestingness.getList")
     fun fetchPhotos(): Call<FlickrResponse>
 
     @GET
     fun fetchUrlBytes(@Url url: String): Call<ResponseBody>
+
+    @GET("services/rest?method=flickr.photos.search")
+    fun searchPhotos(@Query("text") query: String): Call<FlickrResponse>
 }
---
+++ app/src/main/java/com/lengzhang/android/photogallery/api/PhotoInterceptor.kt
@@ -0,0 +1,29 @@
+package com.lengzhang.android.photogallery.api
+
+import okhttp3.HttpUrl
+import okhttp3.Interceptor
+import okhttp3.Request
+import okhttp3.Response
+
+private const val API_KEY = "f608b045255d6b1bc207d7a7b60a7a95"
+
+class PhotoInterceptor : Interceptor {
+
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val originalRequest: Request = chain.request()
+
+        val newUrl: HttpUrl = originalRequest.url().newBuilder()
+            .addQueryParameter("api_key", API_KEY)
+            .addQueryParameter("format", "json")
+            .addQueryParameter("nojsoncallback", "1")
+            .addQueryParameter("extras", "url_s")
+            .addQueryParameter("safesearch", "1")
+            .build()
+
+        val newRequest: Request = originalRequest.newBuilder()
+            .url(newUrl)
+            .build()
+
+        return chain.proceed(newRequest)
+    }
+}
